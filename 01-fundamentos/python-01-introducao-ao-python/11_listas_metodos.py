faf6bca28a0b52c703648567de059a44e9f47af0
Leandro <leandro.machado@cgm.fortaleza.ce.gov.br>
2025-08-13 10:54:59 -0300

    salvando

---
+++ 01-fundamentos/python-01-introducao-ao-python/11_listas_metodos.py
@@ -0,0 +1,13 @@
+"""Utilizando Principais Métodos da Listas."""
+
+game_list = ["Fifa 23", "Star Wars", "The Legend of Zelda", "Red Dead 2"]
+
+# 1 - Tamanho da lista
+print(len(game_list))
+# Saída: 4
+
+# 2 - Recupera o indice do item da lista
+print(game_list.index("Red Dead 2"))
+# Saída: 3
+
+# 3 - Adicionar item ao final da lista
