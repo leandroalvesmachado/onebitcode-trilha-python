42d614d6097a9f13444f12ad11ec647cab1eb56d
Leandro <leandro.machado@cgm.fortaleza.ce.gov.br>
2025-08-14 16:33:56 -0300

    salvando

--- 01-fundamentos/python-01-introducao-ao-python/11_listas_metodos.py
+++ 01-fundamentos/python-01-introducao-ao-python/11_listas_metodos.py
@@ -11,3 +11,23 @@ print(game_list.index("Red Dead 2"))
 # Saída: 3
 
 # 3 - Adicionar item ao final da lista
+game_list.append("GTA V")
+print(game_list)
+# Saída: ['Fifa 23', 'Star Wars', 'The Legend of Zelda', 'Red Dead 2', 'GTA V']
+
+# 4 - Ordenar a lista
+game_list.sort()
+print(game_list)
+# Saída: ['Fifa 23', 'GTA V', 'Red Dead 2', 'Star Wars', 'The Legend of Zelda']
+
+# 5 - Copia os itens de uma lista para outra
+game_reset = game_list.copy()
+game_reset.remove('Fifa 23')
+game_reset.remove('Star Wars')
+print(game_reset)
+# Saída: ['GTA V', 'Red Dead 2', 'The Legend of Zelda']
+
+# 6 - Remove todos os itens da lista
+game_list.clear()
+print(game_list)
+# Saída: []
